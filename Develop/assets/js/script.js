daa5baea65b099931a9a921f4e67541503c92d5b
bpoole53 <bpoole53@gmail.com>
2023-07-03 21:31:20 -0500

    the basic structure has been completed, time to flesh it out

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,49 @@
+// Assignment code here
+
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+var specials = ['!','@','#','$','%','^','&','*','(',')']
+
+if(!stuff.include(variable)) {
+  alert("please choose at least one value");
+  //run choosing of character types function again
+}
+
+var randomTopic = topics[Math.floor(Math.random() * lengthChoice)]
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+function choosePassword()
+
+var lengthChoice = prompt("Please choose password length.")
+var charType1 = prompt("Include lowercase?")
+var charType2 = prompt("Include uppercase?")
+var charType3 = prompt("Include numbers?")
+var charType4 = prompt("Include special characters?")
+
+function generateTypes(length,lower,upper,number,special) {
+  if {length === true} {
+    for (x)
+  }
+}
+
+
+
